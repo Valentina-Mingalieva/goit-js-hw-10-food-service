aa9a21194622cc9b12a24491235ef0b46e2ef96a
Valentina Mingalieva <valentina.mingalieva@gmail.com>
2021-06-12 22:59:48 +0200

    markup done

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import './sass/main.scss';
 import menu from './menu.json';
 import menuItemTpl from './partials/menuItems.hbs';
 
-
-/* console.log(menuItemTpl(menu)); */
-
-/* const menuContainer = document.querySelector('.js-menu');
+/* 
+const menuContainer = document.querySelector('.js-menu');
 const menuMarkup = createMarkup(menu);
 menuContainer.insertAdjacentHTML('beforeend', menuMarkup);
 function createMarkup(menu) {
     return menu.map(menuItemTpl).join('');
-} */
+} 
+*/
 
+const menuContainer = document.querySelector('.js-menu');
 menuContainer.insertAdjacentHTML('beforeend', menuItemTpl(menu));
